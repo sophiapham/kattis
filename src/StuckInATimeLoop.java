1d5d1af58f5cdcc84c1ef2147a8c97535c59a2a4
Sophia Pham <sophia_pham@hotmail.com>
2020-12-01 15:05:19 +0100

    1 more solution

---
+++ src/StuckInATimeLoop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class StuckInATimeLoop {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int i = 0;
+        while(input > i){
+            i++;
+            System.out.println(i + " Abracadabra");
+        }
+
+    }
+}
