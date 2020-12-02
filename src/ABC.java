0d06375ad3918a51cd0db6355798bf856a61bcc7
Sophia Pham <sophia_pham@hotmail.com>
2020-12-02 15:22:53 +0100

    ABC Finished + LuckyDraw not Finished

---
+++ src/ABC.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class ABC {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        //get the current positions
+        int p1 = sc.nextInt();
+        int p2 = sc.nextInt();
+        int p3 = sc.nextInt();
+
+        //sort them by size
+        int pos1 = min(min(p1, p2), p3);
+        int pos3 = max(max(p1, p2), p3);
+        int pos2 = (p1+p2+p3)-pos1-pos3;
+
+        //get the
+        sc.nextLine();
+        String abcOrder = sc.nextLine();
+
+        //Print out the correct order
+        switch (abcOrder) {
+            case "ABC":
+                System.out.println(pos1 + " " + pos2 + " " + pos3);
+                break;
+            case "ACB":
+                System.out.println(pos1 + " " + pos3 + " " + pos2);
+                break;
+                case "BAC":
+                System.out.println(pos2 + " " + pos1 + " " + pos3);
+                break;
+            case "BCA":
+                System.out.println(pos2 + " " + pos3 + " " + pos1);
+                break;
+            case "CAB":
+                System.out.println(pos3 + " " + pos1 + " " + pos2);
+                break;
+            case "CBA":
+                System.out.println(pos3 + " " + pos2 + " " + pos1);
+                break;
+            default:
+                System.out.println(":)");
+        }
+    }
+}
--- src/LuckyDraw.java
+++ src/LuckyDraw.java
@@ -1,20 +1,90 @@
 import java.util.Scanner;
-
+// NOT FINISHED
 public class LuckyDraw {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int players = sc.nextInt();
         int livesOfEachPlayer = sc.nextInt();
         double probabilityOfHeads = sc.nextDouble();
-        double result = 0; //when ends in a draw
+        // For it to be draw: All remaining players need to have tails the last round
 
-        //if tails then -life
-        //if 1 players then win
-        //if all players lose in same round then draw
-        //if winner then win n*bet
-        //if draw then no win (all bets lost)
+        double losingProbability = (double) livesOfEachPlayer * (1-probabilityOfHeads);
 
-        System.out.println(result);
+        double probabilityOfDraw = losingProbability / (double) players;
+
+        //TODO case 1
+        //p1 p2
+        //tail tail 0.5 * 0.5 , 4 kombinationer
+        //tail tail 0.5 * 0.5
+        // 1 / 5.4000000054 = probabilityOfDraw;
+
+        //totalt 11 utfall per games
+
+        //total combinations: 2^n
+        //total combinations to further the game: 2^n-1, where n is players
+        //-1 represent the case where all players have head
+
+        //tail tail
+        //tail head
+
+        //tail tail
+        //head tail
+
+        //head tail
+        //tail head
+        //tail head
+
+        //tail head
+        //head tail
+        //head tail
+
+        //tail head
+        //head tail
+        //tail head
+
+        //head tail
+        //tail head
+        //head tail
+
+        //tail head
+        //tail head
+
+        //head tail
+        //head tail
+
+        //fall 1 / game
+        //tail tail 0.25
+        //tail tail 0.25
+//+
+        //fall 2 / game
+        //head tail 0.25
+        //tail head 0.25
+        //tail tail 0.25
+//+
+        //fall 3 / game
+        //tail head 0.25
+        //head tail 0.25
+        //tail tail 0.25
+
+        //fall1/antamöjligafall + fall2/antamöjigafall + fall3/antalmöjligafall
+
+        //TODO case 2
+        //p1 p2
+        //tail tail 0.2
+        //tail tail 0.2
+
+
+        //TODO case 3
+        //p1 p2 p3 p4 p5
+        //tail tail tail tail tail 0.15
+        //tail tail tail tail tail 0.15
+        //tail tail tail tail tail 0.15
+
+
+        // /sannolikhetenFörTailsFörAlla
+
+
+        System.out.println(probabilityOfDraw);
 
     }
 }
