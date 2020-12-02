017c7fa68a4e1b14067797144f84807117654f2b
Sophia Pham <sophia_pham@hotmail.com>
2020-12-02 16:13:03 +0100

    Cleaner Code Edit

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -16,17 +16,11 @@ public class PigLatin {
     }
     //Convert one word to Pig Latin
     private static String pigLatinConverter(String word){
-        char fstLetter = word.charAt(0);
-        if(fstLetter == 'a' || fstLetter == 'e' ||fstLetter == 'i' ||
-                fstLetter == 'o' || fstLetter == 'u' || fstLetter == 'y'){
-            return word + "yay";
-        }
-        else {
-            for (int i = 0; i < word.length(); i++) {
-                if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' ||
-                        word.charAt(i) == 'o' || word.charAt(i) == 'u' || word.charAt(i) == 'y') {
-                    return word.substring(i) + word.substring(0, i) + "ay";
-                }
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' ||
+                    word.charAt(i) == 'o' || word.charAt(i) == 'u' || word.charAt(i) == 'y') {
+                if (i == 0) return word + "yay";
+                return word.substring(i) + word.substring(0, i) + "ay";
             }
         }
         return word;
